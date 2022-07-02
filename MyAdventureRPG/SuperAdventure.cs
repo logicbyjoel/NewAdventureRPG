9bf33748b4b354c353a96b1c4233c1f92180982e
jgodinez1114 <jgodinez1114@gmail.com>
2022-07-02 18:16:46 -0500

    Add initial labels for testing of controls

--- MyAdventureRPG/SuperAdventure.cs
+++ MyAdventureRPG/SuperAdventure.cs
@@ -16,5 +16,21 @@ namespace MyAdventureRPG
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            // Test out the first button by updating the gold Text associated with the Gold control (shown as a string)
+            lblGold.Text = "123";
+        }
     }
 }
