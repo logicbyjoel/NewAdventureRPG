9a602596e623c0af7e37f680bf362a07f13634a3
jgodinez1114 <jgodinez1114@gmail.com>
2022-07-03 12:11:03 -0500

    add Player class and Player public properties

---
+++ Engine/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    // change access modifer to public
+    public class Player
+    {
+        // declare Player properties 
+        // NOTE: public properties use Pascal casing
+        public int CurrentHitPoints { get; set; }
+        public int MaximumHitPoints { get; set; }
+        public int Gold { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+    }
+}
