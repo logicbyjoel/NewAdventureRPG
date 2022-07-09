36dd4b58884af7437d4f31e27b0d2d7443f0dd9a
jgodinez1114 <jgodinez1114@gmail.com>
2022-07-09 09:33:00 -0500

    add World class for use across the game

---
+++ Engine/World.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public static class World
+    {
+        // instantiate lists for items, bosses, quests, locations
+        public static readonly List<Item> Items = new List<Item>();
+        public static readonly List<Boss> Bosses = new List<Boss>();
+        public static readonly List<Quest> Quests = new List<Quest>();  
+        public static readonly List<Location> Locations = new List<Location>();
+
+        // assign ID's to items, monsters, quests, and locations
+        public const int ITEM_ID_RUSTY_SWORD = 1;
+        public const int ITEM_ID_RAT_TAIL = 2;
+        public const int ITEM_ID_PIECE_OF_FUR = 3;
+        public const int ITEM_ID_SNAKE_FANG = 4;
+        public const int ITEM_ID_SNAKESKIN = 5;
+        public const int ITEM_ID_CLUB = 6;
+        public const int ITEM_ID_HEALING_POTION = 7;
+        public const int ITEM_ID_SPIDER_FANG = 8;
+        public const int ITEM_ID_SPIDER_SILK = 9;
+        public const int ITEM_ID_ADVENTURER_PASS = 10;
+
+        public const int BOSS_ID_RAT = 1;
+        public const int BOSS_ID_SNAKE = 2;
+        public const int BOSS_ID_GIANT_SPIDER = 3;
+
+        public const int QUEST_ID_CLEAR_ALCHEMIST_GARDEN = 1;
+        public const int QUEST_ID_CLEAR_FARMERS_FIELD = 2;
+
+        public const int LOCATION_ID_HOME = 1;
+        public const int LOCATION_ID_TOWN_SQUARE = 2;
+        public const int LOCATION_ID_GUARD_POST = 3;
+        public const int LOCATION_ID_ALCHEMIST_HUT = 4;
+        public const int LOCATION_ID_ALCHEMISTS_GARDEN = 5;
+        public const int LOCATION_ID_FARMHOUSE = 6;
+        public const int LOCATION_ID_FARM_FIELD = 7;
+        public const int LOCATION_ID_BRIDGE = 8;
+        public const int LOCATION_ID_SPIDER_FIELD = 9;
+
+        static World()
+        {
+            PopulateItems();
+            PopulateBosses();
+            PopulateQuests();
+            PopulateLocations();
+        }
+
+        public static void PopulateItems()
+        {
+            Items.Add(new Weapon(ITEM_ID_RUSTY_SWORD, "Rusty sword", "Rusty swords", 0, 5));
+        }
+
+        // TODO: declare functions for PopulateBosses, PopulateQuests, and PopulateLocations
+
+    }
+}
