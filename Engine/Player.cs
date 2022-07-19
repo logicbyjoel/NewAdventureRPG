a3f85dee7d31897ae3d93b3a60de6ea087ab9c42
Joel Godinez <jgodinez1114@gmail.com>
2022-07-19 16:37:28 -0500

    Completed CompletedThisQuest()
    Completed CompletedThisQuest function in Player class as part of refactoring of SuperAdventure.cs

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -60,17 +60,49 @@ namespace Engine
 
         public bool HasThisQuest(Quest quest)
         {
-
+            // find the quest for this destination
+            foreach (PlayerQuest playerQuest in Quests)
+            {
+                if(playerQuest.Details.ID == quest.ID)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool CompletedThisQuest(Quest quest)
         {
-
+            // check if qauest has been completed
+            foreach(PlayerQuest playerQuest in Quests)
+            {
+                // find matching quest to current quest
+                if (playerQuest.Details.ID == quest.ID)
+                {
+                    // return bool of player's quest
+                    return playerQuest.IsCompleted;
+                }
+            }
+            return false;
         }
 
         public bool HasAllQuestCompletionItems(Quest quest)
         {
+            // check if player has all quest completion items required for this quest
+            foreach (QuestCompletionItem qci in quest.QuestCompletionItems)
+            {
+                // assume item will not be found
+                bool foundItemInPlayersInventory = false;
 
+                // check each item in player's inv, to see if they have it, and the minimum quanity
+                foreach (InventoryItem ii in Inventory)
+                {
+                    if(ii.Details.ID == qci.Details.ID) // player has the item in inventory
+                    {
+                        foundItemInPlayersInventory = true;
+                    }
+                }
+            }
         }
 
         public void RemoveQuestCompletionItems(Quest quest)
